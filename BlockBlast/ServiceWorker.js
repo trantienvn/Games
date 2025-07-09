75dc4d13473c169725195afca6fa2c235263a949
Tran Tien <tientn2005@gmail.com>
2025-07-09 13:19:25 +0700

    add flappy

---
+++ BlockBlast/ServiceWorker.js
@@ -0,0 +1,31 @@
+const cacheName = "reun-Block Blast Puzzle-1.1";
+const contentToCache = [
+    "Build/WEB.data",
+    "Build/WEB.framework.js",
+    "Build/WEB.wasm",
+    "Build/WEB.loader.js"
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
