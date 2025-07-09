08d60aaa0cfe6aac3e34822187ce42dcc208904f
Tran Tien <tientn2005@gmail.com>
2025-07-09 12:49:05 +0700

    first commit

---
+++ game.js
@@ -0,0 +1,15 @@
+const GAMES = [
+    {
+        "name": "Flappy",
+        "description": "Trò chơi Flappy Bird cổ điển, điều khiển chim bay qua các ống.",
+        "id": "com.trantien.Flappy",
+        "path": "/Flappy/index.html"
+    },
+    {
+        "name": "Block Blast",
+        "description": "Trò chơi giải đố xếp hình, kết hợp các khối để tạo thành hàng hoàn chỉnh.",
+        "id": "com.trantien.blockblast",
+        "icon": "",
+        "path": "/BlockBlast/index.html"
+    }
+]
